03db646be98e61e949603cf5720d1c62ceac2e13
Gennady Verdel <godrose@gmail.com>
2018-12-17 08:37:37 +0200

    Added support for registrator middlewares

--- src/LogoFX.Server.Bootstrapping/BootstrapperBase.cs
+++ src/LogoFX.Server.Bootstrapping/BootstrapperBase.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Solid.Bootstrapping;
 using Solid.Common;
 using Solid.Extensibility;
@@ -10,40 +9,30 @@ namespace LogoFX.Server.Bootstrapping
     /// <summary>
     /// Defines basic bootstrapper for server initialization.
     /// </summary>
-    public partial class BootstrapperBase : IInitializable,
-        IExtensible<BootstrapperBase>,     
+    public partial class BootstrapperBase : 
+        IInitializable,        
         IHaveRegistrator<IServiceCollection>,
         IHaveErrors
-    {
-        private readonly
-            List<IMiddleware<BootstrapperBase>>
-            _middlewares =
-                new List<IMiddleware<BootstrapperBase>>();
-
+    {       
         /// <inheritdoc />
         protected BootstrapperBase(IServiceCollection dependencyRegistrator)
         {
             Registrator = dependencyRegistrator;
             PlatformProvider.Current = new NetStandardPlatformProvider();
+            _concreteMiddlewaresWrapper = new MiddlewaresWrapper<BootstrapperBase>(this);
+            _registratorMiddlewaresWrapper = new MiddlewaresWrapper<IHaveRegistrator<IServiceCollection>>(this);
         }
 
         /// <inheritdoc />
-        public IServiceCollection Registrator { get; }
-
-        /// <inheritdoc />
-        public BootstrapperBase Use(
-            IMiddleware<BootstrapperBase> middleware)
-        {
-            _middlewares.Add(middleware);
-            return this;
-        }
+        public IServiceCollection Registrator { get; }       
 
         /// <inheritdoc />
         public void Initialize()
         {            
             CreateAssemblies();
             InitializeCompositionModules();
-            MiddlewareApplier.ApplyMiddlewares(this, _middlewares);
+            MiddlewareApplier.ApplyMiddlewares(this, _concreteMiddlewaresWrapper.Middlewares);
+            MiddlewareApplier.ApplyMiddlewares(this, _registratorMiddlewaresWrapper.Middlewares);
         }
     }        
 }
