055464e323d80bd35e22ecfff43155526c56f377
Gennady Verdel <godrose@gmail.com>
2018-12-26 18:24:52 +0200

    Using aspect-based bootstrapper

--- src/LogoFX.Server.Bootstrapping/BootstrapperBase.cs
+++ src/LogoFX.Server.Bootstrapping/BootstrapperBase.cs
@@ -1,38 +1,81 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 using Solid.Bootstrapping;
 using Solid.Common;
 using Solid.Extensibility;
+using Solid.Practices.Composition;
+using Solid.Practices.Composition.Contracts;
 using Solid.Practices.Middleware;
+using Solid.Practices.Modularity;
 
 namespace LogoFX.Server.Bootstrapping
 {
-    /// <summary>
-    /// Defines basic bootstrapper for server initialization.
-    /// </summary>
-    public partial class BootstrapperBase : 
-        IInitializable,        
-        IHaveRegistrator<IServiceCollection>,
-        IHaveErrors
-    {       
-        /// <inheritdoc />
-        protected BootstrapperBase(IServiceCollection dependencyRegistrator)
-        {
+    public class BootstrapperBase :
+         IInitializable,
+         IExtensible<BootstrapperBase>,
+         IExtensible<IHaveRegistrator<IServiceCollection>>,
+         IHaveAspects<BootstrapperBase>,
+         ICompositionModulesProvider,
+         IHaveRegistrator<IServiceCollection>, 
+         IAssemblySourceProvider
+    {
+        private ModularityAspect _modularityAspect;
+        private DiscoveryAspect _discoveryAspect;
+        private readonly ExtensibilityAspect<IHaveRegistrator<IServiceCollection>> _registratorExtensibilityAspect;
+        private readonly ExtensibilityAspect<BootstrapperBase> _concreteExtensibilityAspect;
+        private readonly AspectsCollection _aspectsCollection = new AspectsCollection();
+
+        protected BootstrapperBase(IServiceCollection dependencyRegistrator) : this() =>
             Registrator = dependencyRegistrator;
-            PlatformProvider.Current = new NetStandardPlatformProvider();
-            _concreteMiddlewaresWrapper = new MiddlewaresWrapper<BootstrapperBase>(this);
-            _registratorMiddlewaresWrapper = new MiddlewaresWrapper<IHaveRegistrator<IServiceCollection>>(this);
+
+        public IServiceCollection Registrator { get; }
+
+        private BootstrapperBase()
+        {
+            _concreteExtensibilityAspect = new ExtensibilityAspect<BootstrapperBase>(this);
+            _registratorExtensibilityAspect = new ExtensibilityAspect<IHaveRegistrator<IServiceCollection>>(this);
         }
 
-        /// <inheritdoc />
-        public IServiceCollection Registrator { get; }       
+        public IEnumerable<Assembly> Assemblies => _discoveryAspect.Assemblies;
 
-        /// <inheritdoc />
+        IEnumerable<ICompositionModule> ICompositionModulesProvider<ICompositionModule>.Modules =>
+            _modularityAspect.Modules;
+
+        public BootstrapperBase Use(IMiddleware<BootstrapperBase> middleware)
+        {
+            return _concreteExtensibilityAspect.Use(middleware);
+        }
+
+        public IHaveRegistrator<IServiceCollection> Use(IMiddleware<IHaveRegistrator<IServiceCollection>> middleware)
+        {
+            return _registratorExtensibilityAspect.Use(middleware);
+        }
+        
+        public virtual ModularityOptions ModularityOptions => new ModularityOptions();
+
         public void Initialize()
-        {            
-            CreateAssemblies();
-            InitializeCompositionModules();
-            MiddlewareApplier.ApplyMiddlewares(this, _concreteMiddlewaresWrapper.Middlewares);
-            MiddlewareApplier.ApplyMiddlewares(this, _registratorMiddlewaresWrapper.Middlewares);
+        {
+            _aspectsCollection.UseCoreAspects(CreateCoreAspects());
+            _aspectsCollection.Initialize();
         }
-    }        
+
+        private IEnumerable<IAspect> CreateCoreAspects()
+        {
+            var aspects = new List<IAspect> { new PlatformAspect() };
+            _modularityAspect = new ModularityAspect(ModularityOptions);
+            aspects.Add(_modularityAspect);
+            _discoveryAspect = new DiscoveryAspect(ModularityOptions);
+            aspects.Add(_discoveryAspect);
+            aspects.Add(_concreteExtensibilityAspect);
+            aspects.Add(_registratorExtensibilityAspect);
+            return aspects;
+        }
+
+        public BootstrapperBase UseAspect(IAspect aspect)
+        {
+            _aspectsCollection.UseAspect(aspect);
+            return this;
+        }        
+    }
 }
