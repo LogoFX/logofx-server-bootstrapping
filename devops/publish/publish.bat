81633b1e7d6c4f05f0532f15c9207d519ceb32a3
Gennady Verdel <godrose@gmail.com>
2021-04-13 20:14:40 +0300

    chore: bump Solid.* deps

--- devops/publish/publish.bat
+++ devops/publish/publish.bat
@@ -1,6 +1,8 @@
 SET package_name=LogoFX.Server.Bootstrapping
 SET package_version=2.2.0-rc2
-cd ../build
+cd ../../src
+nuget restore
+cd ../devops/build
 call build.bat
 cd ../pack
 call ./pack.bat
